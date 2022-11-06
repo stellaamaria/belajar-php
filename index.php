052d55c9facdf81d82feb437ec80ddaf3ddd6466
Stella Maria Matulessy <lalamatulessy@gmail.com>
2022-11-06 23:11:47 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "Hello World!<br>";
+
+$nama = "Stella Maria";
+$umur = 21;
+
+echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+$namaAdik = "Stefan Kevin";
+$umurAdik = 15;
+
+echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun.<br>";
+
+$selisihUmur = $umur - $umurAdik;
+echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun.";
+
+?>
